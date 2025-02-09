cb80cade984caed6b0ce102113eac515b6c62055
Akash1191 <104644145+Akash1191@users.noreply.github.com>
2025-02-09 22:09:54 +0530

    Your commit message here

--- agrihub-frontend/src/AgriHub.js
+++ agrihub-frontend/src/AgriHub.js
@@ -1,40 +1,404 @@
-// src/AgriHub.js
 import Web3 from 'web3';
-import AgriHubABI from './AgriHubABI.json'; // Import your contract ABI
+
+// Contract ABI and address
+const contractABI = [
+    [
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "productName",
+              "type": "string"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "fixedPrice",
+              "type": "uint256"
+            }
+          ],
+          "name": "FixedPriceUpdated",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "farmer",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "quantity",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "pricePerUnit",
+              "type": "uint256"
+            }
+          ],
+          "name": "ProductAdded",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "productName",
+              "type": "string"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "farmer",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "quantity",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "fixedPrice",
+              "type": "uint256"
+            }
+          ],
+          "name": "ProductAddedToHub",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "productName",
+              "type": "string"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "fromFarmer",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "toFarmer",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "quantity",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "fixedPrice",
+              "type": "uint256"
+            }
+          ],
+          "name": "ProductTransferred",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            },
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "name": "hubProducts",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [],
+          "name": "priceCeiling",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [],
+          "name": "priceFloor",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "productBids",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "quantity",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "pricePerUnit",
+              "type": "uint256"
+            },
+            {
+              "internalType": "address",
+              "name": "farmer",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "productList",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "products",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "totalQuantity",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "fixedPrice",
+              "type": "uint256"
+            },
+            {
+              "internalType": "bool",
+              "name": "isFixedPriceSet",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_quantity",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_pricePerUnit",
+              "type": "uint256"
+            }
+          ],
+          "name": "addProduct",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_quantity",
+              "type": "uint256"
+            }
+          ],
+          "name": "addProductToHub",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_name",
+              "type": "string"
+            },
+            {
+              "internalType": "address",
+              "name": "_toFarmer",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "_quantity",
+              "type": "uint256"
+            }
+          ],
+          "name": "transferProduct",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "_farmer",
+              "type": "address"
+            }
+          ],
+          "name": "getFarmerProducts",
+          "outputs": [
+            {
+              "internalType": "string[]",
+              "name": "",
+              "type": "string[]"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "",
+              "type": "uint256[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function",
+          "constant": true
+        }
+      ]
+];
+const contractAddress = '0x12C882ec07028771B86FB1e9b22907D8B71FD4dC';
 
 let web3;
-let agriHubContract;
+let contract;
 
 export const connectWallet = async () => {
-    if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const contractAddress = "0x143DAe7be86c0Ca9dA18E3C6507e45a638232646"; // Your deployed contract address
-        agriHubContract = new web3.eth.Contract(AgriHubABI, contractAddress);
-        return true; // Wallet connected
-    } else {
-        alert('Please install MetaMask!');
-        return false; // Wallet not connected
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      contract = new web3.eth.Contract(contractABI, contractAddress);
+      return true;
+    } catch (error) {
+      console.error('Wallet connection failed:', error);
+      return false;
     }
+  } else {
+    alert('Please install MetaMask!');
+    return false;
+  }
 };
 
-// The rest of your functions (addProduct, addProductToHub, etc.) remain unchanged
-
-export const addProduct = async (name, quantity, pricePerUnit) => {
-    const accounts = await web3.eth.getAccounts();
-    return await agriHubContract.methods.addProduct(name, quantity, pricePerUnit).send({ from: accounts[0] });
+export const addProduct = async (productName, quantity, pricePerUnit) => {
+  const accounts = await web3.eth.getAccounts();
+  await contract.methods.addProduct(productName, quantity, pricePerUnit).send({
+    from: accounts[0],
+  });
 };
 
-export const addProductToHub = async (name, quantity) => {
-    const accounts = await web3.eth.getAccounts();
-    return await agriHubContract.methods.addProductToHub(name, quantity).send({ from: accounts[0] });
+export const addProductToHub = async (productName, quantity) => {
+  const accounts = await web3.eth.getAccounts();
+  await contract.methods.addProductToHub(productName, quantity).send({
+    from: accounts[0],
+  });
 };
 
-export const transferProduct = async (name, toFarmer, quantity) => {
-    const accounts = await web3.eth.getAccounts();
-    return await agriHubContract.methods.transferProduct(name, toFarmer, quantity).send({ from: accounts[0] });
+export const transferProduct = async (productName, toFarmer, quantity) => {
+  const accounts = await web3.eth.getAccounts();
+  await contract.methods.transferProduct(productName, toFarmer, quantity).send({
+    from: accounts[0],
+  });
 };
 
 export const getFarmerProducts = async (farmerAddress) => {
-    return await agriHubContract.methods.getFarmerProducts(farmerAddress).call();
+  const products = await contract.methods.getFarmerProducts(farmerAddress).call();
+  return products;
 };
--- agrihub-frontend/src/App.js
+++ agrihub-frontend/src/App.js
@@ -1,89 +1,123 @@
-// src/App.js
 import React, { useState } from 'react';
 import { connectWallet, addProduct, addProductToHub, transferProduct, getFarmerProducts } from './AgriHub';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 function App() {
-    const [productName, setProductName] = useState('');
-    const [quantity, setQuantity] = useState(0);
-    const [pricePerUnit, setPricePerUnit] = useState(0);
-    const [farmerProducts, setFarmerProducts] = useState([]);
-    const [farmerAddress, setFarmerAddress] = useState('');
-    const [account, setAccount] = useState('');
+  const [productName, setProductName] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [pricePerUnit, setPricePerUnit] = useState(0);
+  const [farmerProducts, setFarmerProducts] = useState([]);
+  const [farmerAddress, setFarmerAddress] = useState('');
+  const [account, setAccount] = useState('');
 
-    const handleConnectWallet = async () => {
-        const connected = await connectWallet();
-        if (connected) {
-            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-            setAccount(accounts[0]);
-        }
-    };
+  const handleConnectWallet = async () => {
+    const connected = await connectWallet();
+    if (connected) {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      setAccount(accounts[0]);
+    }
+  };
 
-    const handleAddProduct = async () => {
-        try {
-            await addProduct(productName, quantity, pricePerUnit);
-            alert('Product added successfully!');
-        } catch (error) {
-            console.error("Error adding product:", error);
-            alert('Failed to add product. Check console for details.');
-        }
-    };
+  const handleAddProduct = async () => {
+    try {
+      await addProduct(productName, quantity, pricePerUnit);
+      alert('Product added successfully!');
+    } catch (error) {
+      console.error('Error adding product:', error);
+      alert('Failed to add product. Check console for details.');
+    }
+  };
 
-    const handleAddToHub = async () => {
-        try {
-            await addProductToHub(productName, quantity);
-            alert('Product added to hub successfully!');
-        } catch (error) {
-            console.error("Error adding to hub:", error);
-            alert('Failed to add to hub. Check console for details.');
-        }
-    };
+  const handleAddToHub = async () => {
+    try {
+      await addProductToHub(productName, quantity);
+      alert('Product added to hub successfully!');
+    } catch (error) {
+      console.error('Error adding to hub:', error);
+      alert('Failed to add to hub. Check console for details.');
+    }
+  };
 
-    const handleTransferProduct = async (toFarmer) => {
-        try {
-            await transferProduct(productName, toFarmer, quantity);
-            alert('Product transferred successfully!');
-        } catch (error) {
-            console.error("Error transferring product:", error);
-            alert('Failed to transfer product. Check console for details.');
-        }
-    };
+  const handleTransferProduct = async (toFarmer) => {
+    try {
+      await transferProduct(productName, toFarmer, quantity);
+      alert('Product transferred successfully!');
+    } catch (error) {
+      console.error('Error transferring product:', error);
+      alert('Failed to transfer product. Check console for details.');
+    }
+  };
 
-    const handleGetFarmerProducts = async () => {
-        try {
-            const products = await getFarmerProducts(farmerAddress);
-            setFarmerProducts(products);
-        } catch (error) {
-            console.error("Error fetching farmer products:", error);
-            alert('Failed to fetch products. Check console for details.');
-        }
-    };
+  const handleGetFarmerProducts = async () => {
+    try {
+      const products = await getFarmerProducts(farmerAddress);
+      setFarmerProducts(products);
+    } catch (error) {
+      console.error('Error fetching farmer products:', error);
+      alert('Failed to fetch products. Check console for details.');
+    }
+  };
 
-    return (
-        <div className="container mt-5">
-            <h1>AgriHub</h1>
-            <button className="btn btn-primary mb-3" onClick={handleConnectWallet}>
-                {account ? `Connected: ${account}` : 'Connect Wallet'}
-            </button>
-            <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Product Name" value={productName} onChange={(e) => setProductName(e.target.value)} />
-                <input type="number" className="form-control mt-2" placeholder="Quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
-                <input type="number" className="form-control mt-2" placeholder="Price per Unit (in wei)" value={pricePerUnit} onChange={(e) => setPricePerUnit(e.target.value)} />
-                <button className="btn btn-success mt-2" onClick={handleAddProduct}>Add Product</button>
-                <button className="btn btn-warning mt-2" onClick={handleAddToHub}>Add to Hub</button>
-            </div>
-            <div className="mb-3">
-                <input type="text" className="form-control" placeholder="Farmer Address" value={farmerAddress} onChange={(e) => setFarmerAddress(e.target.value)} />
-                <button className="btn btn-info mt-2" onClick={handleGetFarmerProducts}>Get Farmer Products</button>
-            </div>
-            <h2>Farmer Products</h2>
-            <ul>
-                {farmerProducts[0] && farmerProducts[0].map((name, index) => (
-                    <li key={index}>{name}: {farmerProducts[1][index]} units</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="container mt-5">
+      <h1>AgriHub</h1>
+      <button className="btn btn-primary mb-3" onClick={handleConnectWallet}>
+        {account ? `Connected: ${account}` : 'Connect Wallet'}
+      </button>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Product Name"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+        />
+        <input
+          type="number"
+          className="form-control mt-2"
+          placeholder="Quantity"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <input
+          type="number"
+          className="form-control mt-2"
+          placeholder="Price per Unit (in wei)"
+          value={pricePerUnit}
+          onChange={(e) => setPricePerUnit(e.target.value)}
+        />
+        <button className="btn btn-success mt-2" onClick={handleAddProduct}>
+          Add Product
+        </button>
+        <button className="btn btn-warning mt-2" onClick={handleAddToHub}>
+          Add to Hub
+        </button>
+      </div>
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Farmer Address"
+          value={farmerAddress}
+          onChange={(e) => setFarmerAddress(e.target.value)}
+        />
+        <button className="btn btn-info mt-2" onClick={handleGetFarmerProducts}>
+          Get Farmer Products
+        </button>
+      </div>
+      <h2>Farmer Products</h2>
+      <ul>
+        {farmerProducts[0] &&
+          farmerProducts[0].map((name, index) => (
+            <li key={index}>
+              {name}: {farmerProducts[1][index]} units
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
